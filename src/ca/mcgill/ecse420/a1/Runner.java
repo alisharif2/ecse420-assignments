a75a9b9fe9e809905abb93d2bf5a7b1e2f09cbc0
Ebou <ebou.jobe@mail.mcgill.ca>
2018-10-10 12:39:37 -0400

    deadlock explanation

--- src/ca/mcgill/ecse420/a1/Runner.java
+++ src/ca/mcgill/ecse420/a1/Runner.java
@@ -14,42 +14,36 @@ public class Runner {
 
     //don't hold several locks at once. If you do, always acquire the locks in the same order
     //try to get the both locks
-    private void acquireLocks(Lock firstLock, Lock secondLock) throws InterruptedException {
-        while (true) {
-            // Acquire locks
-            boolean gotFirstLock = false;
-            boolean gotSecondLock = false;
-            try {
-                /**
-                 * tryLock() which will only acquire a lock if it’s available
-                 * and not already acquired by another thread and tryLock(long
-                 * time,TimeUnit unit), which will try to acquire a lock and, if
-                 * it's unavailable wait for the specified timer to expire
-                 * before giving up
-                 */
-                gotFirstLock = firstLock.tryLock();
-                gotSecondLock = secondLock.tryLock();
-            } finally {
-                if (gotFirstLock && gotSecondLock) return;
-                else if (gotFirstLock) firstLock.unlock();
-                else if (gotSecondLock) secondLock.unlock();
-            }
-            // Locks not acquired
-            Thread.sleep(1);
-        }
-    }
+//    private void acquireLocks(Lock firstLock, Lock secondLock) throws InterruptedException {
+//        while (true) {
+//            // Acquire locks
+//            boolean gotFirstLock = false;
+//            boolean gotSecondLock = false;
+//            try {
+//                /**
+//                 * tryLock() which will only acquire a lock if it’s available
+//                 * and not already acquired by another thread and tryLock(long
+//                 * time,TimeUnit unit), which will try to acquire a lock and, if
+//                 * it's unavailable wait for the specified timer to expire
+//                 * before giving up
+//                 */
+//                gotFirstLock = firstLock.tryLock();
+//                gotSecondLock = secondLock.tryLock();
+//            } finally {
+//                if (gotFirstLock && gotSecondLock) return;
+//                else if (gotFirstLock) firstLock.unlock();
+//                else if (gotSecondLock) secondLock.unlock();
+//            }
+//            // Locks not acquired
+//            Thread.sleep(1);
+//        }
+//    }
 
     //firstThread runs
     public void firstThread() throws InterruptedException {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            acquireLocks(lock1, lock2);
-            try {
                 Account.transfer(acc1, acc2, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
         }
     }
 
@@ -57,13 +51,7 @@ public class Runner {
     public void secondThread() throws InterruptedException {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            acquireLocks(lock2, lock1);
-            try {
                 Account.transfer(acc2, acc1, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
         }
     }
 
