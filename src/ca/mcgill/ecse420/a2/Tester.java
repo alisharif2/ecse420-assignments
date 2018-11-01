9e5710b85c1682b00724ff2beb2fc793e3e11ce8
Ali <ali.sharif2@mail.mcgill.ca>
2018-11-01 19:05:56 -0400

    Use executor pool in tester instead of individual thread objects

--- src/ca/mcgill/ecse420/a2/Tester.java
+++ src/ca/mcgill/ecse420/a2/Tester.java
@@ -1,15 +1,22 @@
 package ca.mcgill.ecse420.a2;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Tester {
   
   private Lock f;
   private int n;
+  private final ExecutorService thread_pool;
+  
   public boolean use_penalty;
   
   public Tester(Lock f, int n) {
     this.f = f;
     this.n = n;
     this.use_penalty = false;
+    this.thread_pool = Executors.newFixedThreadPool(n);
   }
   
   public void execute_test() {
@@ -18,15 +25,16 @@ public class Tester {
     TestThreadJob.run_mutex_test();
     
     TestThreadJob jobs[] = new TestThreadJob[n];
-    Thread[] t = new Thread[n];
+    
     // Create thread objects to execute runnable jobs
     for(int i = 0;i < n;++i) {
       // Create each job with an execution penalty proportional to id
-      if(use_penalty) jobs[i] = new TestThreadJob(i, f, (short)(i+1));
-      else jobs[i] = new TestThreadJob(i, f);
+      if(use_penalty) 
+        jobs[i] = new TestThreadJob(i, f, (short)(i+1));
+      else 
+        jobs[i] = new TestThreadJob(i, f);
       
-      t[i] = new Thread(jobs[i]); // create thread objects to execute the jobs
-      t[i].start();
+      thread_pool.execute(jobs[i]);
     }
 
     // Let the test run for a while
@@ -37,13 +45,21 @@ public class Tester {
     }
 
     TestThreadJob.alive = false; // end all threads
-    for(int i = 0;i < n;++i) {
-      try {
-        t[i].join(); // make sure all jobs have ended
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+    thread_pool.shutdown();
+    try {
+      if(!thread_pool.awaitTermination(1000, TimeUnit.MILLISECONDS))
+      {
+        thread_pool.shutdownNow();
+        if(!thread_pool.awaitTermination(1000, TimeUnit.MILLISECONDS))
+        {
+          System.out.println("Could not shtudown threads");
+        }
       }
+    } catch (InterruptedException e) {
+      thread_pool.shutdownNow();
+      e.printStackTrace();
     }
+    
     
     System.out.println("How many times each job entered it's critical section");
     for(int i = 0;i < n;++i) {
