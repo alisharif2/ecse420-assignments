af80620dd5161743de6f888f9f13efdf5767000d
Ali <ali.sharif2@mail.mcgill.ca>
2018-12-03 11:28:12 -0500

    Changed new parallelized multiplier to use generic matrices instead of column vectors

--- src/ca/mcgill/ecse420/a3/SimpleParallizedMultiplier.java
+++ src/ca/mcgill/ecse420/a3/SimpleParallizedMultiplier.java
@@ -6,49 +6,54 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class SimpleParallizedMultiplier {
-  
-  private static ExecutorService exec = Executors.newFixedThreadPool(MatrixTester.N_THREADS_DEFAULT);
-  
-  public static Matrix mult(SquareMatrix a, ColumnVector b) throws InterruptedException, ExecutionException {
-    if(a.col_size != b.row_size) return null;
-    ColumnVector c = new ColumnVector(b.row_size);
-    
-    Future<?>[] f = new Future<?>[a.row_size];
-    for(int i = 0;i < a.row_size;++i) {
-      f[i] = exec.submit(new Task(a, b, c, i));
+
+  public static ExecutorService exec =
+      Executors.newFixedThreadPool(MatrixTester.N_THREADS_DEFAULT);
+
+  public static Matrix mult(Matrix a, Matrix b) throws InterruptedException, ExecutionException {
+    if (a.col_size != b.row_size)
+      return null;
+    Matrix c = new Matrix(a.row_size, b.col_size);
+
+    Future<?>[][] f = new Future<?>[a.row_size][b.col_size];
+    for (int i = 0; i < a.row_size; ++i) {
+      for (int j = 0; j < b.col_size; ++j) {
+        f[i][j] = exec.submit(new Task(a, b, c, i, j));
+      }
     }
-    
-    for(int i = 0;i < a.row_size;++i) {
-      f[i].get();
+
+    for (int i = 0; i < a.row_size; ++i) {
+      for (int j = 0; j < b.col_size; ++j) {
+        f[i][j].get();
+      }
     }
-    
+
     return c;
   }
-  
+
   private static class Task implements Runnable {
 
-    SquareMatrix a;
-    ColumnVector b, c;
-    int row;
-    
-    public Task(SquareMatrix a, ColumnVector b, ColumnVector c, int row) {
+    Matrix a, b, c;
+    int row, col;
+
+    public Task(Matrix a, Matrix b, Matrix c, int row, int col) {
       this.a = a;
       this.b = b;
       this.c = c;
       this.row = row;
+      this.col = col;
     }
-    
+
     @Override
     public void run() {
-      c.set(row, 0, 0);
-      for(int i = 0;i < a.col_size;++i) {
-        c.set(row, 0, c.get(row, 0) + a.get(row, i) * b.get(i, 0));
+      c.set(row, col, 0);
+      for (int i = 0; i < a.col_size; ++i) {
+        c.set(row, col, c.get(row, col) + a.get(row, i) * b.get(i, col));
       }
     }
-    
+
   }
-  
-  
-  
+
+
 
 }
